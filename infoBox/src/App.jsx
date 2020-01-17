0b856be9c0b234f16f917c92dda8c25aae34727e
valeriia <lera.iordaki@gmail.com>
2020-01-17 18:04:40 +0200

    infoBox

---
+++ infoBox/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import InfoBox from './infoBox.jsx';
+
+export default class App extends React.Component{
+    render(){
+       return(
+        <InfoBox />
+       )
+    }
+}
